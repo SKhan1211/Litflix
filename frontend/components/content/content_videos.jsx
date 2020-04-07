53391b59e823745f440cf2ef17539a727aba2056
Suhaib <suhaib92khan@gmail.com>
2020-04-07 18:59:22 -0400

    Commit before push to heroku

--- frontend/components/content/content_videos.jsx
+++ frontend/components/content/content_videos.jsx
@@ -48,16 +48,29 @@ class ContentVideos extends React.Component {
       <div className="content-row-container">
         <h1>{this.props.title}</h1>
         <div className="video-container">
-          {/* {
+          {
             this.props.movies.map((movie, idx) => (
               <Link to="#" className="img-link-1">
                 <img src={movie.photoURL} alt="Lorem Ipsum" className="row-1-col-1" onMouseEnter={this.handleOnHover} onMouseLeave={this.clear} onClick={() => this.props.history.push(`/watch/${movie.id}`)} />
-                <video muted autoPlay loop width="100" id="first-vid" className="mini-video-player-blank" onMouseLeave={this.handleOffHover} src={movie.videoURL}></video>
+                <div className="img-link-vid-container-hidden" onMouseLeave={this.handleOffHover}>
+                  <video muted autoPlay loop width="100" id="first-vid" className="mini-video-player" onMouseLeave={this.handleOffHover} src={movie.videoURL}></video>
+                  <div id="img-link-text" className="img-link-text-container">
+                    <Link to="#"><i class="far fa-play-circle" onClick={() => this.props.history.push(`/watch/${movie.id}`)}></i></Link>
+                    <div className="img-link-info-container">
+                      <div>
+                        <h1>{movie.title}</h1>
+                        <h2>{movie.rating}, {movie.length}</h2>
+                        <p>{movie.genre}</p>
+                      </div>
+                      <button><i class="fas fa-plus"></i></button>
+                    </div>
+                  </div>
+                </div>
               </Link>
             ))
-          } */}
+          }
 
-          <Link to="#" className="img-link-1">
+          {/* <Link to="#" className="img-link-1">
             <img src={"https://assets.justinmind.com/wp-content/uploads/2018/11/Lorem-Ipsum-alternatives.png"} alt="Lorem Ipsum" className="row-1-col-1" onMouseEnter={this.handleOnHover} onMouseLeave={this.clear} />
             <div className="img-link-vid-container-hidden" onMouseLeave={this.handleOffHover}>
               <video muted autoPlay loop width="100" id="first-vid" className="mini-video-player" onMouseLeave={this.handleOffHover} src={"https://ak4.picdn.net/shutterstock/videos/1014118994/preview/stock-footage-an-ancient-wooden-tablet-filled-with-carved-runic-signs-d-panning-from-top-left-to-bottom-right.webm"}></video>
@@ -158,7 +171,7 @@ class ContentVideos extends React.Component {
                 </div>
               </div>
             </div>
-          </Link>
+          </Link> */}
         </div>
       </div>
     )
